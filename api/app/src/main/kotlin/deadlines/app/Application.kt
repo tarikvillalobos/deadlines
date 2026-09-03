6be350295a4b181d2584fedf7a1351d70ee92878
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-02 22:44:08 -0300

    feat: add GET /health route

---
+++ api/app/src/main/kotlin/deadlines/app/Application.kt
@@ -0,0 +1,19 @@
+package deadlines.app
+
+import deadlines.app.plugins.configureSerialization
+import deadlines.app.routes.configureRouting
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+private const val DEFAULT_PORT = 8080
+
+fun main() {
+    val port = System.getenv("PORT")?.toIntOrNull() ?: DEFAULT_PORT
+    embeddedServer(Netty, port = port, module = Application::module).start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
