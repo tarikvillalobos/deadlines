d4aed9680c89dbcbf0eea3962edc5120a688c4b1
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-09-06 12:24:39 -0300

    refactor: restore email login form

--- web/features/identity/presentation/screens/LoginScreen.tsx
+++ web/features/identity/presentation/screens/LoginScreen.tsx
@@ -1,16 +1,45 @@
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
-import { Field, FieldGroup } from "@/components/ui/field";
+import { Field, FieldDescription, FieldGroup } from "@/components/ui/field";
 import { AuthShell } from "@/features/identity/presentation/components/AuthShell";
+import { AuthTextField } from "@/features/identity/presentation/components/AuthTextField";
 
 export function LoginScreen() {
   return (
-    <AuthShell title="Welcome to Deadlines" description="Continue to access your workspace.">
+    <AuthShell title="Login to your account" description="Enter your email below to login to your account.">
       <form>
         <FieldGroup>
+          <AuthTextField
+            id="email"
+            label="Email"
+            type="email"
+            autoComplete="email"
+            placeholder="m@example.com"
+            required
+          />
+          <AuthTextField
+            id="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            required
+            action={
+              <Link href="/forgot-password" className="text-sm underline-offset-4 hover:underline">
+                Forgot your password?
+              </Link>
+            }
+          />
           <Field>
-            <Button className="w-full" variant="outline" type="button">
-              Continue with Google
+            <Button className="w-full" type="button">
+              Login
             </Button>
+            <FieldDescription className="text-center">
+              Don&apos;t have an account?{" "}
+              <Link href="/register" className="underline-offset-4 hover:underline">
+                Sign up
+              </Link>
+            </FieldDescription>
           </Field>
         </FieldGroup>
       </form>
